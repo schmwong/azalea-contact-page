2dd5cf1413143cbd924f71932f5fdf1389d53775
schmwong <smwong87@gmail.com>
2022-09-19 14:11:46 +0800

    initial angular commit

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, ContactFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
